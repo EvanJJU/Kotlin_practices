1fb390ba3d4f30ae3133491ba525c2aefb29d894
EvanJJU <jee1303@konkuk.ac.kr>
2023-12-27 21:16:13 +0900

    자료형과 형변환 소스 추가

--- kotlinProj/src/KotlinTest/TypeConversion.kt
+++ kotlinProj/src/KotlinTest/TypeConversion.kt
@@ -1,6 +1,32 @@
 package KotlinTest
 
 class TypeConversion {
+//     println("Hello, world!!!")
 
-    val test1 = 1234
+//     val test1 = "1234"
+//     val test2 = 1234
+
+//     println(test1 + 1)
+// 	println(test2 + 1)
+
+// 	println(test1::class.java.simpleName)
+//     println(test2::class.java.simpleName)
+
+//     val test3 = 1.32
+// //     println(test3::class.java.simpleName)
+//     val t4 : Int = 1234
+//     println(t4)
+
+//     val test9 : Int = 1234
+//     val test10 = test9.toString()
+//     println(test10::class.java.simpleName)
+
+//     val test11 : String = "1234"
+//     val test12 = Integer.parseInt(test11)
+//     println(test12::class.java.simpleName)
+//     println(test12+9999999)
+
+    val test16 : String? = "null"
+    val test17 : String? = null
+    
 }
