ed8b50c3f95ea6013a8dbd0ca3187c420df01cf5
EvanJJU <jee1303@konkuk.ac.kr>
2023-12-28 19:54:53 +0900

    If & When

---
+++ kotlinProj/src/KotlinTest/IfAndWhen.kt
@@ -0,0 +1,62 @@
+package KotlinTest
+
+class IfAndWhen {
+
+//     val studentScore1 = 100
+//     if (studentScore1 > 150){
+//         println("150 이상")
+//     }
+//     else {
+//         println("150 이상이 아님")
+//     }
+
+//     val studentScore2 = 50
+//     if (studentScore2 > 50){
+//         println("50을 초과")
+//     }
+
+//     if (studentScore2 >= 50){
+//         println("50 이상")
+//     }
+
+//     val studentScore3 = 65
+
+//     if(studentScore3 <= 70){
+//         println("70이하")
+//     }
+
+//     if(studentScore3 < 70){
+//         println("70미만")
+//     }
+
+//     val studentScore4 = 40
+
+//     if (studentScore4 > 100) {
+//         println("100보다 큼")
+//     } else if (studentScore4 > 50) {
+//         println("50보다 큼")
+//     } else if (studentScore4 > 30) {
+//         println("30보다 큼")
+//     }  else {
+//             println("30보다 작음")
+//         }
+
+//    val score = 1
+//    when(score) {
+//        100 -> {
+//            println("100")
+//        }
+//        90 -> {
+//            println("90")
+//        }
+//        80 -> {
+//            println("80")
+//        }
+//        else -> {
+//            print("X")
+//        }
+//    }
+
+
+
+}
